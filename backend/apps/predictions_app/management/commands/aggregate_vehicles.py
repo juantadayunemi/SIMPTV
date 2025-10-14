37a6df58159345bb05f7a5b78bd51e3152a6dc9e
aadrianavilas <aavilasanchez922@gmail.com>
2025-10-14 11:20:47 -0500

    Aggregate Vehicle At 10 Minutes

--- backend/apps/predictions_app/management/commands/aggregate_vehicles.py
+++ backend/apps/predictions_app/management/commands/aggregate_vehicles.py
@@ -1,5 +1,10 @@
 from django.core.management.base import BaseCommand
 from apps.predictions_app.tasks import aggregate_prediction_data
+"""
+Comando para ejecutar la tarea de agregación manualmente:
+python manage.py aggregate_vehicles
+"""
+
 class Command(BaseCommand):
     help = 'Agrupa vehículos en bloques de 10 minutos'
 
