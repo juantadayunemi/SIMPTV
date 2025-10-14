de731087a92e8eee9a4531bc38b66b72e8bc8514
aadrianavilas <aavilasanchez922@gmail.com>
2025-10-13 21:51:54 -0500

    Changes Run Schedule Tasks

--- backend/apps/predictions_app/tasks.py
+++ backend/apps/predictions_app/tasks.py
@@ -4,26 +4,6 @@ from django.db.models import Avg, Max, Count
 from apps.traffic_app.models import Vehicle
 from apps.predictions_app.models import PredictionSource
 
-
-class PredictionDataAggregator:
-    """
-    redis-server : para ejecutar el servidor de Redis
-    celery -A config.celery worker -l info : para iniciar el worker de Celery
-    celery -A config.celery beat -l info : para iniciar el scheduler de Celery (planificador de tareas)
-    """
-
-    def __init__(self):
-        pass
-
-    def group_data_by_time(self):
-        """Agrupa los datos por intervalos de tiempo específicos."""
-        pass
-
-    def celery_task(self):
-        """Tarea de Celery para ejecutar la agregación de datos periódicamente."""
-        pass
-
-
 @shared_task
 def aggregate_prediction_data():
     """Agrupa vehículos por cámara, ubicación y bloque de tiempo (10 minutos)."""
@@ -46,7 +26,7 @@ def aggregate_prediction_data():
         )
 
         if vehicles.exists():
-            # 🔸 Agrupamos según la cámara y ubicación del análisis asociado
+            # Agrupamos según la cámara y ubicación del análisis asociado
             grouped_data = vehicles.values(
                 "trafficAnalysis__cameraId", "trafficAnalysis__locationId"
             ).annotate(totalVehicleCount=Count("id"), avgSpeed=Avg("avgSpeed"))
